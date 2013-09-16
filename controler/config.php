c666abee9c644b28058bcbd1d0c0f75f067a3c24
praise <praise.cross@gmail.com>
2013-09-16 18:20:27 +0800

    add error reporting process

--- controler/config.php
+++ controler/config.php
@@ -6,4 +6,7 @@
  * Time: 8:20 PM
  * To change this template use File | Settings | File Templates.
  */
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+date_default_timezone_set('Asia/Shanghai');
 ?>
