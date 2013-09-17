2211b1b8707151b8e27c969a2e8a208e52e2c972
praise <praise.cross@gmail.com>
2013-09-17 23:17:24 +0800

    add form validate

--- controler/add.php
+++ controler/add.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" name="viewport" />
+    <meta content="black" name="apple-mobile-web-app-status-bar-style" />
+    <meta content="telephone=no" name="format-detection" />
+    <title>添加商品----商品进存系统</title>
+    <link rel="stylesheet" href="static/css/pure-min.css" />
+    <link rel="stylesheet" href="static/css/app.css" />
+    <script src="static/js/zepto.min.js"></script>
+</head>
+<body>
+
 <?php
 include_once('config.php');
 include_once('db.php');
@@ -48,93 +62,7 @@ if($writed_product){
 }else{
     echo $error_msg;
 }
-
-//$author = @$_POST['author'];
-//$type = @$_POST['type'];
-//$thumb = @$_POST['thumb'];
-//
-//$created_tem_dir = false;
-//
-//if(!$js){
-//    $js = "该模版没有js代码";
-//}
-//
-//if(!$tms){
-//    $tms = "该模版没有tms代码";
-//}
-//
-//function getCategories(){
-//    global $dpl_dir;
-//    $handle = fopen($dpl_dir.'/'."categories.md", 'r');
-//    $buffers = array();
-//    $len = filesize($dpl_dir.'/'."categories.md");
-//    if((int)$len < 2){
-//        return $buffers;
-//    }
-//    if ($handle) {
-//        while (($buffer = fgets($handle, $len)) !== false) {
-//            if(strlen(trim($buffer)) > 2){
-//                $type = preg_split("/\s/", $buffer);
-//                array_push($buffers, json_encode(array("name"=>$type[1], "id"=>$type[0])));
-//            }
-//        }
-//        if (!feof($handle)) {
-//            echo "靠！发生了怪异的事情，找颂赞吧";
-//        }
-//        fclose($handle);
-//    }
-//    return $buffers;
-//}
 ?>
 
-<?php
-//新增分类
-//if($action == "add_type"){
-//    $type_id = @$_POST['id'];
-//    if(!$name){
-//        exit("缺少新增分类的name");
-//    }
-//    if(!$type_id){
-//        exit("缺少新增分类的id");
-//    }
-//    foreach(getCategories() as $k => $category){
-//        $class = "";
-//        if($k === 0){
-//            $class = "active";
-//        }
-//        $category = json_decode($category);
-//        if($category->name === $name){
-//            exit("分类名称已存在");
-//        }else if($category->id === $type_id){
-//            exit("分类id已存在");
-//        }
-//    }
-//
-//    $cate_handle = fopen($dpl_dir."/categories.md", 'a');
-//    $writed = fwrite($cate_handle, "\n".$type_id." ".$name);
-//
-//    if($writed){
-//        exit("success");
-//    }else{
-//        exit("新增分类失败");
-//    }
-//
-//    exit;
-//}
-//
-////检查模版文件夹是否重名
-//if($action == "check_tem_name"){
-//    if(is_dir($dpl_dir."/".$en_name)){
-//        echo "yes";
-//    }else{
-//        echo "no";
-//    }
-//    exit;
-//}
-//
-////生成随机文件名
-//if($action == "generate_temName"){
-//    $date = new DateTime();
-//    exit(md5($date->format('Y-m-d H:i:s')));
-//}
-?>
+</body>
+</html>
--- static/js/add.js
+++ static/js/add.js
@@ -33,13 +33,13 @@ $(function (){
     var addProps = function (){
         var html = '<form class="pure-form addProps"><fieldset>'+
                         '<div class="pure-control-group">'+
-                            '<label for="name">属性名称</label>'+
-                            '<input id="name" type="text" required="required">'+
+                            '<label>属性名称</label>'+
+                            '<input type="text" placeholder="如：颜色" id="prop_name">'+
                         '</div>'+
                         '<div class="pure-control-group">'+
-                            '<label for="name">属性值</label>'+
+                            '<label>属性值</label>'+
                             '<div class="valuesContainer">'+
-                                '<div class="valueBox"><input type="text" /></div>'+
+                                '<div class="valueBox"><input type="text" placeholder="如：红色" /></div>'+
                                 '<a href="javascript:void(0)" class="J-addValue">增加一个属性值</a>' +
                             '</div>'+
                         '</div>'+
@@ -80,7 +80,7 @@ $(function (){
                 zIndex: 101,
                 width: "auto",
                 '-webkit-border-radius': "10px",
-                '-webkit-box-shadow': '3px 3px 3px #666'
+                '-webkit-box-shadow': '3px 3px 3px rgb(25, 25, 25)'
             });
         }
         function closePop(){
@@ -112,13 +112,20 @@ $(function (){
                 bindUI();
             });
             $('.addProps .ok').unbind().bind('click', function (e){
-                var name = $.trim($('.addProps #name').val());
+                e.preventDefault();
+                var name = $.trim($('.addProps #prop_name').val());
                 var values = [];
                 var valueBoxes = $('.valuesContainer input[type=text]');
                 var propsContainer = $('.J-props');
                 $.each(valueBoxes, function (i, box){
                     values.push($.trim($(box).val()));
                 });
+                if(!name){
+                    $('.addProps #prop_name').focus();
+                    return alert("属性名称不能为空");
+                }else if(!values.join('')){
+                    return alert('属性值不能为空');
+                }
                 if(propsContainer.find('.prop').length <= 0){
                     propsContainer.html('');
                 }
@@ -155,6 +162,59 @@ $(function (){
     addBtn.on('click', function (e){
         e.preventDefault();
         addProps();
+    });
+
+    $('#J-submit').click(function (e){
+        var pNameNode = $('#name');
+        var pPriceNode = $('#price');
+        var pCount = $('#count');
+        var from = $('input[name=from]');
+        var man = $('input[name=man]');
+        var photo = $('#J-takePhoto');
+        var hasFrom = false;
+        var hasMan = false;
+
+        if(!$.trim(pNameNode.val())){
+            alert('商品不能为空');
+            pNameNode.focus();
+            return false;
+        }else if(!$.trim(pPriceNode.val())){
+            pPriceNode.focus();
+            alert('单价不能为空');
+            return false;
+        }else if(!$.trim(pCount.val())){
+            pCount.focus();
+            alert('数量不能为空');
+            return false;
+        }else if(!/^\d+$/.test($.trim(pCount.val()))){
+            pCount.select().focus();
+            alert('数量必须为数字');
+            return false;
+        }
+
+        $.each(from, function (i, f){
+            if($(f).attr('checked')){
+                hasFrom = true;
+                return false;
+            }
+        });
+        $.each(man, function (i, f){
+            if($(f).attr('checked')){
+                hasMan = true;
+                return false;
+            }
+        });
+
+        if(!hasMan){
+            alert('必须选择一个采购人');
+            return false;
+        }else if(!hasFrom){
+            alert('必须选择一个采购源');
+            return false;
+        }else if(!$.trim(photo.val())){
+            alert('请上传商品图片');
+            return false;
+        }
     });
 });
 
