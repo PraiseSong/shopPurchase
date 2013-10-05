b7a4ec6b832e9fb72d482babefe8d93cc3d576bd
praise <praise.cross@gmail.com>
2013-10-05 12:41:00 +0800

    隐藏iPhone滚动条

--- templates/header.php
+++ templates/header.php
@@ -7,8 +7,9 @@ include_once($libs_dir."/Mobile_Detect.php")
 <head>
     <meta charset="utf-8" />
     <meta content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" name="viewport" />
-    <meta content="black" name="apple-mobile-web-app-status-bar-style" />
     <meta content="telephone=no" name="format-detection" />
+    <meta content="yes" name="apple-mobile-web-app-capable" />
+    <meta content="black" name="apple-mobile-web-app-status-bar-style" />
     <link href="static/imgs/logo.png" rel="apple-touch-icon-precomposed" />
     <title>
         <?php
