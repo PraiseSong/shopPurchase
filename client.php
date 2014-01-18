a09a7572f5d05e2d34234fcef1b39189ba9c2784
Praise <praise.cross@gmail.com>
2014-01-18 11:30:30 +0800

    修复一些安全问题

--- client.php
+++ client.php
@@ -20,10 +20,12 @@ $client_api = @$_POST['api'];
 $client_action = @$_POST['action'];
 $client_data = @$_POST['data'];
 $client_username = @$_POST['username'];
+$localpassword = @$_POST['localpassword'];
 
 if(!$client_username){
     exit(json_encode(array("bizCode" => 0, "memo" => "$client_api : 没有username参数", "data" => array())));
 }
+
 if(!$client_action){
     exit(json_encode(array("bizCode" => 0, "memo" => "$client_api : 没有action参数", "data" => array())));
 }
@@ -32,6 +34,15 @@ if(!$client_api){
 }
 
 $loggedInUser = logining(fetchUserDetails($client_username));
+
+if($client_api !== 'login.php' && $client_api !== "register.php"
+   && $client_api !== 'account_settings.php' && $client_api !== 'forgot.php' && $client_api !== 'help.php'
+   && $client_api !== 'update_password.php' && $client_api !== 'login.php'){
+    if(!$localpassword || ($loggedInUser -> hash_pw !== $localpassword)){
+        $data = array("bizCode"=>4,"memo"=>"安全验证不通过，请退出后重新登录","data"=>array());
+        exit(json_encode($data));
+    }
+}
 
 require_once("controlers/$client_api");
 ?>
--- controlers/userAuth.php
+++ controlers/userAuth.php
@@ -10,16 +10,6 @@ include_once($libs_dir.'/db.php');
 require_once("models/config.php");
 
 if($client_action === "update"){
-    $password = @$_POST['password'];
-
-    if(!$password){
-        $password = '';
-    }
-
-    if(!$password || ($loggedInUser -> hash_pw !== $password)){
-        $data = array("bizCode"=>0,"memo"=>"验证不通过，用户未登录","data"=>array());
-        exit(json_encode($data));
-    }
     if(isset($loggedInUser) && $loggedInUser->user_id){
         $data = array("bizCode"=>1,"memo"=>"","data"=>array("user" => $loggedInUser));
         exit(json_encode($data));
